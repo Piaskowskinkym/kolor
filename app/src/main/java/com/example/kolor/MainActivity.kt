0a07d0972f2c5b9a7c385e67928c2442dc00a41a
Nekoyama <piaskowski3456@gmail.com>
2021-11-08 15:54:22 +0100

    dodanie czerwonego tintu

---
+++ app/src/main/java/com/example/kolor/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.kolor
+
+import android.graphics.*
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ToggleButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var red = findViewById<ToggleButton>(R.id.tBred)
+        var blue = findViewById<ToggleButton>(R.id.tBblue)
+        var green = findViewById<ToggleButton>(R.id.tBgreen)
+        var image = findViewById<ImageView>(R.id.iVork)
+        
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.pobrane)
+        val bitmapCzerwony = BitmapFactory.decodeResource(resources, R.drawable.czerwony)
+        val filterBitmap = Bitmap.createBitmap(bitmap.width,bitmap.height,Bitmap.Config.ARGB_8888)
+
+      /*  red.setOnClickListener {
+            val paint = Paint()
+            paint.alpha = 100
+            val canvas = Canvas(filterBitmap)
+            canvas.drawARGB(0,0,0,0)
+            canvas.drawBitmap(bitmapCzerwony, null, RectF(0f,0f,bitmap.width.toFloat(),bitmap.height.toFloat()),paint)
+            canvas.drawBitmap(bitmap,0f,0f,paint)
+            image.setImageBitmap(filterBitmap)
+        }*/
+        red.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val paint = Paint()
+                paint.alpha = 100
+                val canvas = Canvas(filterBitmap)
+                canvas.drawARGB(0, 0, 0, 0)
+                canvas.drawBitmap(
+                    bitmapCzerwony,
+                    null,
+                    RectF(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat()),
+                    paint
+                )
+                canvas.drawBitmap(bitmap, 0f, 0f, paint)
+                image.setImageBitmap(filterBitmap)
+            } else {
+                image.setImageBitmap(bitmap)
+            }
+        }
+        blue.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val paint = Paint()
+                paint.alpha = 100
+                val canvas = Canvas(filterBitmap)
+                canvas.drawARGB(0, 0, 0, 0)
+                canvas.drawBitmap(
+                    bitmapCzerwony,
+                    null,
+                    RectF(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat()),
+                    paint
+                )
+                canvas.drawBitmap(bitmap, 0f, 0f, paint)
+                image.setImageBitmap(filterBitmap)
+            } else {
+                image.setImageBitmap(bitmap)
+            }
+
+        }
+    }
+
+}
